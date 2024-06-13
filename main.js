5c64b7bfc47b4434f73adbdd1cd821c7bfb3e452
Kaosi Nwakoby <kenwakoby@gmail.com>
2024-06-13 10:44:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://github.com/kaosinwakoby'
         }
     }
 })
